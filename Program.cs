3892ccbf48609854771dc9714da6f6f804d8d3d9
Артем Саблев <zobiko62@gmail.com>
2024-04-26 16:10:09 +0300

    result -> priceAfterDiscount

--- Program.cs
+++ Program.cs
@@ -3,29 +3,29 @@
     {
         public decimal CalculateDiscount(decimal amount, int type, int years)
         {
-            decimal result = 0;
+            decimal priceAfterDiscount = 0;
             decimal disc = (years > 5) ? (decimal)5 / 100 : (decimal)years / 100;
             if (type == 1)
             {
-                result = amount;
+                priceAfterDiscount = amount;
             }
             else if (type == 2)
             {
-                result = (amount - (0.1m * amount)) - disc * (amount - (0.1m * amount));
+                priceAfterDiscount = (amount - (0.1m * amount)) - disc * (amount - (0.1m * amount));
             }
             else if (type == 3)
             {
-                result = (0.7m * amount) - disc * (0.7m * amount);
+                priceAfterDiscount = (0.7m * amount) - disc * (0.7m * amount);
             }
             else if (type == 4)
             {
-                result = (amount - (0.5m * amount)) - disc * (amount - (0.5m * amount));
+                priceAfterDiscount = (amount - (0.5m * amount)) - disc * (amount - (0.5m * amount));
             }
             else
             {
                 return -1;
             }
-            return result;
+            return priceAfterDiscount;
         }
 
         public static void Main() 
